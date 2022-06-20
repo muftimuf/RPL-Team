f11821385a4508cc2e13c9a72c40eee96a47135a
muftimuf <tiimuf.personal@gmail.com>
2022-06-20 22:36:09 +0700

    download bootstrap

--- js/script.js
+++ js/script.js
@@ -4,25 +4,8 @@ VanillaTilt.init(document.querySelectorAll(".foto-wrap"), {
   speed: 400,
   glare: true,
   gyroscope: true,
+  gyroscopeMinAngleX: -45,
+  gyroscopeMaxAngleX: 45,
+  gyroscopeMinAngleY: -45,
+  gyroscopeMaxAngleY: 45,
 });
-
-// Momentum Scroll
-const body = document.body,
-  scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
-  height = scrollWrap.getBoundingClientRect().height - 1,
-  speed = 0.04;
-
-var offset = 0;
-
-body.style.height = Math.floor(height) + "px";
-
-function smoothScroll() {
-  offset += (window.pageYOffset - offset) * speed;
-
-  var scroll = "translateY(-" + offset + "px) translateZ(0)";
-  scrollWrap.style.transform = scroll;
-
-  callScroll = requestAnimationFrame(smoothScroll);
-}
-
-smoothScroll();
