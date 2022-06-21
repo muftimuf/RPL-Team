922cf79ed4490924ae935279e148b54dd01155ce
muftimuf <tiimuf.personal@gmail.com>
2022-06-21 08:45:22 +0700

    menghapus preloader

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// Preloader
-var loader = document.querySelector(".loader");
-var konten = document.querySelector(".konten");
-
-window.addEventListener("load", vanish);
-
-function vanish() {
-  loader.classList.add("disappear");
-  konten.classList.add("overflow-scroll");
-}
-
 // Tilt JS
 VanillaTilt.init(document.querySelectorAll(".foto-wrap"), {
   max: 10,
